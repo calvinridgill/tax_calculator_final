2ba321f91c63dd976d996a4b3b395f49d990c5eb
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-03-25 15:43:03 +0300

    create a localStorage module

---
+++ src/utils/localStorage.js
@@ -0,0 +1,13 @@
+export default class LocalStorage {
+  static getItem(key) {
+    const value = localStorage.getItem(key);
+    return JSON.parse(value);
+  }
+  static setItem(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  static clear() {
+    localStorage.clear();
+  }
+}
