1c145a0b61c36682bd71dc3a67c28a9372520eeb
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-04-02 13:56:09 +0300

    create a signin page UI

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,8 +1,112 @@
 import React from "react";
+import { Box, Paper, TextField, Typography, Button } from "@mui/material";
+import { InputAdornment } from "@mui/material";
+import EmailIcon from "@mui/icons-material/Email";
+import LockIcon from "@mui/icons-material/Lock";
+import { Form, useNavigation } from "react-router-dom";
+import { sleep } from "../utils";
+
 export function Signin() {
+  const navigation = useNavigation();
   return (
-    <div>
-      <h1>Signin</h1>
-    </div>
+    <Form method="post">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: {
+            xs: "column-reverse",
+            md: "row",
+          },
+        }}
+      >
+        <Box sx={{ flex: 1, height: "92vh", p: 2 }}>
+          <Typography>Some info about the company</Typography>
+        </Box>
+        <Box
+          sx={{
+            bgcolor: "#eee",
+            flex: 1,
+            height: "92vh",
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          {/* layout element start */}
+          <Box
+            sx={{
+              height: "150px",
+            }}
+          />
+          {/* layout element end */}
+          <Paper
+            elevation={3}
+            sx={{
+              p: 5,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              gap: 2,
+              width: "90%",
+              maxWidth: 400,
+              mb: 4,
+            }}
+          >
+            <Typography variant="h4">Welcome</Typography>
+            <TextField
+              variant="outlined"
+              type="email"
+              placeholder="Enter your email"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <EmailIcon />
+                  </InputAdornment>
+                ),
+              }}
+              sx={{
+                bgcolor: "#e7f0fe",
+              }}
+            />
+            <TextField
+              variant="outlined"
+              type="password"
+              placeholder="Enter your password"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <LockIcon />
+                  </InputAdornment>
+                ),
+              }}
+              sx={{
+                bgcolor: "#e7f0fe",
+              }}
+            />
+            <Button
+              sx={{
+                textTransform: "none",
+                maxWidth: 200,
+                mt: 3,
+              }}
+              variant="contained"
+              fullWidth
+              size="large"
+              disabled={navigation.state === "submitting"}
+              type="submit"
+            >
+              Login
+            </Button>
+          </Paper>
+        </Box>
+      </Box>
+    </Form>
   );
 }
+
+export const action = async () => {
+  await sleep(2000);
+  return null;
+};
--- src/routes/loggedOutRoutes.jsx
+++ src/routes/loggedOutRoutes.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { PageNotFound } from "../components/PageNotFound";
 import { LoggedOutAppBar } from "../components/LoggedOutAppBar";
-import { Signin } from "../pages/Signin";
+import { Signin, action as loginAction } from "../pages/Signin";
 import { Signup } from "../pages/Signup";
 import Testing from "../pages/Testing";
 
 export const loggedOutRoutes = (
   <Route element={<LoggedOutAppBar />}>
-    <Route path="/signin" element={<Signin />} />
+    <Route path="/signin" element={<Signin />} action={loginAction} />
     <Route path="/signup" element={<Signup />} />
     <Route path="/offering" element={<div>This is the offering page</div>} />
     <Route
