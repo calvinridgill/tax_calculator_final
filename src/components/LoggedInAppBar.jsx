152ce8ff07e93e0ff9a079c694b38a89ac6c476c
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-05-23 04:48:53 +0300

    update loggedin App Bar

--- src/components/LoggedInAppBar.jsx
+++ src/components/LoggedInAppBar.jsx
@@ -7,6 +7,7 @@ import {
   MenuItem,
   Menu,
   Box,
+  Container,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 
@@ -29,46 +30,57 @@ export function LoggedInAppBar() {
   };
   return (
     <>
-      <AppBar position="static">
-        <Toolbar>
-          <Box sx={{ flex: 1 }}>
-            <Link to="/app" style={{ textDecoration: "none", color: "white" }}>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                Tax Calculator
-              </Typography>
-            </Link>
-          </Box>
-          <div>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleClose}>Profile</MenuItem>
-              <MenuItem onClick={handleLogout}>Log out</MenuItem>
-            </Menu>
-          </div>
-        </Toolbar>
+      <AppBar position="relative" color="transparent" enableColorOnDark>
+        <Container>
+          <Toolbar sx={{ gap: { xs: 1, md: 2 }, p: 0 }}>
+            <Box
+              component="img"
+              src="/images/tax-calculate.png"
+              width="40px"
+              height="40px"
+            />
+            <Box sx={{ flex: 1 }}>
+              <Link
+                to="/app"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                  Tax Calculator
+                </Typography>
+              </Link>
+            </Box>
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="primary"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleLogout}>Log out</MenuItem>
+              </Menu>
+            </div>
+          </Toolbar>
+        </Container>
       </AppBar>
     </>
   );
