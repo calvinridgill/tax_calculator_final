720c618180a8bdc874206352fead65cc9f1a97e9
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-06-19 17:56:17 +0300

    create a Hero section of the landing page

--- src/components/LandingPage/Hero.jsx
+++ src/components/LandingPage/Hero.jsx
@@ -1,112 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { Box, Container, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import React from "react";
+import { Box, Container, Typography, IconButton } from "@mui/material";
 import styles from "./hero.module.css";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 export function Hero() {
-  const [moveTitleBar, setTitleBar] = useState(false);
-  useEffect(() => {
-    function handleScroll() {
-      if (window.scrollY > 60) setTitleBar(true);
-      else setTitleBar(false);
-    }
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <Box>
-      <Box sx={{ position: "relative", zIndex: 2 }}>
-        <Box
-          sx={[
-            {
-              height: "60px",
-              background: "rgba(0,0,0,0.2)",
-              position: "fixed",
-              top: 0,
-              left: 0,
-              right: 0,
-            },
-            moveTitleBar && { display: "none" },
-          ]}
-        />
-        <Box
-          sx={[
-            {
-              position: "fixed",
-              top: 60,
-              transition: "top 0.5s ease",
-              width: "100%",
-            },
-            moveTitleBar && { background: "#ededed", top: 0 },
-          ]}
-        >
-          <Container
-            maxWidth="md"
-            sx={{
-              left: 0,
-              display: "flex",
-            }}
-          >
-            <Box
-              sx={{
-                flex: 4,
-                display: "flex",
-                gap: 3,
-                justifyContent: "center",
-                pt: 2,
-                pb: 2,
-              }}
-            >
-              <Box
-                sx={[
-                  moveTitleBar && {
-                    position: "relative",
-                    display: "flex",
-                    alignItems: "center",
-                    "&::after": {
-                      content: '""',
-                      position: "absolute",
-                      bottom: "-17px",
-                      width: 0,
-                      height: 0,
-                      borderLeft: "7px solid transparent",
-                      borderRight: "7px solid transparent",
-                      borderBottom: "7px solid black",
-                      opacity: 1,
-                      left: "50%",
-                      marginLeft: " -10px",
-                    },
-                  },
-                ]}
-              >
-                <Link style={{ textDecoration: "none" }}>
-                  <Typography
-                    variant="body2"
-                    sx={[
-                      { color: "white" },
-                      moveTitleBar && { color: "black" },
-                    ]}
-                  >
-                    HOME
-                  </Typography>
-                </Link>
-              </Box>
-              <Link style={{ textDecoration: "none" }} to="/app">
-                <Typography
-                  variant="body2"
-                  sx={[{ color: "white" }, moveTitleBar && { color: "black" }]}
-                >
-                  TAX PREP TOOL
-                </Typography>
-              </Link>
-            </Box>
-            <Box sx={{ flex: 1 }} />
-          </Container>
-        </Box>
-      </Box>
       <Box component={"section"} sx={{ position: "relative" }}>
         <Box sx={{ height: 1, position: "absolute", left: 0, right: 0 }}>
           <Box
@@ -141,17 +40,18 @@ export function Hero() {
               maxWidth: {
                 xs: "100%",
                 sm: "80%",
-                md: "760px",
-                lg: "960px",
-                xl: "1024px",
+                md: "960px",
+                lg: "1024px",
               },
-              pt: 30,
+              pt: {
+                xs: 20,
+                sm: 30,
+              },
             }}
           >
             <Box>
               {/* title */}
               <Box sx={{ color: "white" }}>
-                {" "}
                 <Typography
                   variant="h2"
                   sx={{
@@ -168,8 +68,8 @@ export function Hero() {
                   variant="h5"
                   sx={{
                     fontSize: "24px !important",
-                    mt: 6,
-                    mb: 6,
+                    mt: 3,
+                    mb: 3,
                     textAlign: "center",
                     textShadow: "0 0 18px rgba(0,0,0,0.5)",
                     fontFamily: "Lato, sans-serif",
@@ -182,29 +82,46 @@ export function Hero() {
               </Box>
               {/* call for action */}
               <Box
-                sx={{
+                sx={(theme) => ({
                   display: "flex",
                   justifyContent: "center",
                   alignItems: "center",
-                }}
+                  pb: 5,
+                  zIndex: 20,
+                  [theme.breakpoints.down("sm")]: {
+                    position: "fixed",
+                    pb: 0,
+                    bottom: 5,
+                    right: 5,
+                  },
+                })}
               >
-                <Box
-                  sx={{
-                    display: "flex",
-                    p: 1,
-                    bgcolor: "white",
-                    borderRadius: "50%",
-                    fontSize: 40,
-                  }}
-                >
+                <IconButton>
                   <Box
-                    component="span"
-                    sx={{ fontSize: "40px" }}
-                    className="material-symbols-outlined"
+                    sx={{
+                      display: "flex",
+                      p: 1,
+                      bgcolor: "white",
+                      borderRadius: "50%",
+                      width: 60,
+                      height: 60,
+                      boxShadow: "1px 1px 10px rgba(0,0,0,0.5)",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      "&:hover": {
+                        bgcolor: "#343434",
+                      },
+                      "&:hover #shopping-cart-icon": {
+                        fill: "white",
+                      },
+                    }}
                   >
-                    mail
+                    <ShoppingCartIcon
+                      id="shopping-cart-icon"
+                      sx={{ fill: "black" }}
+                    />
                   </Box>
-                </Box>
+                </IconButton>
               </Box>
             </Box>
           </Container>
