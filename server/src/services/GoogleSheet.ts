b1dbb72e13c21bc8bf66e6f29f62901362027a66
Devesh <devesh.tiwari@wunderkind.co>
2024-04-09 12:29:35 +0530

    first commit

--- server/src/services/GoogleSheet.ts
+++ server/src/services/GoogleSheet.ts
@@ -34,7 +34,7 @@ export class GoogleSheet {
   }
 
   public createGoogleSheet = async (
-    title = "Tax Calculator New",
+    title = "Tax Calculator",
   ): Promise<string> => {
     const spreadsheet = await this.googleSheets.spreadsheets.create({
       requestBody: {
@@ -85,7 +85,7 @@ export class GoogleSheet {
             updateSheetProperties: {
               properties: {
                 sheetId: newSheetId,
-                title: "Tax Calculator New",
+                title: "Tax Calculator",
               },
               fields: "title",
             },
