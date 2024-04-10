2e471778dcd15c4bd7d434e2dd82d33e93896cc7
Devesh <devesh.tiwari@wunderkind.co>
2024-04-10 16:31:06 +0530

    first commited

--- server-old/src/routes/paymentRoutes.ts
+++
@@ -1,6 +0,0 @@
-import express from "express"
-import { createCheckoutSession } from "../controllers/paymentController"
-
-export const paymentRouter = express.Router()
-
-paymentRouter.post("/create_checkout_session", createCheckoutSession)
--- src-old/components/dashboard/UserDashboard.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function MessageDashboard() {
-  return <div>This is the user dashboard component</div>;
-}
