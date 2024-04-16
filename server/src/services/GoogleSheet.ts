72b5b7d1768b38e32d75629b0badc2869758fba2
Devesh <devesh.tiwari@wunderkind.co>
2024-04-16 12:16:11 +0530

    first commited

--- server/src/services/GoogleSheet.ts
+++ server/src/services/GoogleSheet.ts
@@ -1,5 +1,6 @@
 import { google, sheets_v4, drive_v3 } from "googleapis";
 import { currentEnvConfig } from "../models/config";
+import { Product } from "../models/product"; // Import your Product model
 
 export class GoogleSheet {
   private static client;
@@ -14,7 +15,6 @@ export class GoogleSheet {
     this.originalSpreadSheetId = currentEnvConfig.ORIGINAL_SPREADSHEET_ID;
   }
 
-  // Method to initialize the Google Sheets client
   private static async initializeClient() {
     if (!this.client) {
       const auth = new google.auth.GoogleAuth({
@@ -29,13 +29,11 @@ export class GoogleSheet {
     }
   }
 
-  // Method to create an instance of GoogleSheet class
   static async createInstance() {
     await GoogleSheet.initializeClient();
     return new GoogleSheet();
   }
 
-  // Method to create a new Google Sheet
   public async createGoogleSheet(title = "Tax Calculator"): Promise<string> {
     const spreadsheet = await this.googleSheets.spreadsheets.create({
       requestBody: {
@@ -52,16 +50,14 @@ public async copyTaxCalculatorContent(
   originalSpreadSheetId?: string,
   newSpreadSheetId?: string
 ): Promise<string> {
-  // Check if originalSpreadSheetId is provided, if not use the default one
+
   if (!originalSpreadSheetId && !this.originalSpreadSheetId)
     throw new Error("originalSpreadSheetId is not defined");
 
   originalSpreadSheetId = originalSpreadSheetId || this.originalSpreadSheetId;
 
-  // If newSpreadSheetId is not provided, create a new sheet
   newSpreadSheetId = newSpreadSheetId || (await this.createGoogleSheet());
 
-  // Copy content from the original sheet to the new sheet
   const response = await this.googleSheets.spreadsheets.sheets.copyTo({
     spreadsheetId: originalSpreadSheetId,
     sheetId: 0,
@@ -70,36 +66,37 @@ public async copyTaxCalculatorContent(
     },
   });
 
-  // Get the new sheet id
   const newSheetId = response.data.sheetId;
 
-  // Clear the existing data from the new sheet
   await this.googleSheets.spreadsheets.values.clear({
     spreadsheetId: newSpreadSheetId,
     range: "Sheet1!A1:Z",
   });
 
+  const products = await Product.find({});
+
   const customData = [
-    ["Income", ""],
-    ["Gross Income", 9000],
-    ["", ""],
-    ["Expense", ""],
-    ["Gas", 50],
-    ["Supplies", 10],
-    ["Cell Phone", 5],
-    ["Auto insurance", 10],
-    ["Office expense", 5],
-    ["All other expenses", 15],
-    ["Commissions and fees", 5],
-    ["Auto lease or note payment", 2],
-    ["Auto Repairs and maintenance", 4],
-    ["Legal and professional services", 5],
-                  
-    ["", ""],
-    ["Net income", 1000],
+    ["Product Name", "Income", "Expense"],
   ];
 
-  // Add the new data to the new sheet
+  products.forEach(product => {
+    const rowData: string[] = [
+        product.name,
+        product.income.toString(),
+        product.gas.toString(),
+        product.supplies.toString(),
+        product.cell_phone.toString(),
+        product.auto_insurance.toString(),
+        product.office_expense.toString(),
+        product.other_expenses.toString(),
+        product.commissions_fees.toString(),
+        product.auto_lease_note_payment.toString(),
+        product.auto_repairs_maintenance.toString(),
+        product.legal_professional_services.toString(),
+    ];
+    customData.push(rowData);
+});
+
   await this.googleSheets.spreadsheets.values.update({
     spreadsheetId: newSpreadSheetId,
     range: "Sheet1!A1",
@@ -109,14 +106,11 @@ public async copyTaxCalculatorContent(
     },
   });
 
-  // Add writer permission for the new user
   await this.addWriterPermission(newSpreadSheetId, newUserEmail);
 
-  // Return the URL of the new sheet
   return `https://docs.google.com/spreadsheets/d/${newSpreadSheetId}/edit#gid=${newSheetId}`;
 }
 
-  // Method to add writer permission to a sheet
   private async addWriterPermission(
     spreadsheetId: string,
     emailAddress: string
