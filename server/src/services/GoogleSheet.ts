1cc3a628147c1500fbaf9d5d56984783e71d2742
Devesh <devesh.tiwari@wunderkind.co>
2024-04-17 18:44:35 +0530

    first commit

--- server/src/services/GoogleSheet.ts
+++ server/src/services/GoogleSheet.ts
@@ -134,11 +134,6 @@ export class GoogleSheet {
       requestBody: {
         requests: [
           {
-            deleteSheet: {
-              sheetId: 1,
-            },
-          },
-          {
             repeatCell: {
               range: {
                 sheetId: 0,
